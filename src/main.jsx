7137cf38a7f1cb6063e8beeda039e2574c8c6fd1
Guilheme Vasconcelos <seuchicolive@gmail.com>
2025-11-19 09:20:22 -0300

    martelando...

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-// ... outras importações ...
-import { HashRouter } from 'react-router-dom'; // ✅ Nova Importação
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+import './index.css' 
+
+
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter> {/* ✅ Novo Componente */}
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
-);
+)
